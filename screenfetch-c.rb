6505fb691b33d36913b7ddc059c1b26bb42c6dea
William Woodruff <william@tuffbizz.com>
2014-05-30 23:14:23 -0400

    experimentation

--- screenfetch-c.rb
+++ screenfetch-c.rb
@@ -2,11 +2,12 @@ require "formula"
 
 class ScreenfetchC < Formula
 	homepage "http://woodruffw.us/realsite/projects/screenfetch-c.html"
+	version "1.2"
 	url "https://github.com/woodruffw/screenfetch-c/archive/1.2-fix.tar.gz"
 	sha1 "8d21049fde9a27a1a895ccbd1d782fd28f3908d5"
 
 	def install
-		system "make", "osx"
-		system "make", "install"
+		system "make", "osx", "prefix=#{prefix}"
+		system "make", "prefix=#{prefix}"
 	end
 end
