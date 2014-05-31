a8d32ea60cfd0c567195c770444159f555bca22f
William Woodruff <william@tuffbizz.com>
2014-05-30 23:31:37 -0400

    continued testing

--- screenfetch-c.rb
+++ screenfetch-c.rb
@@ -7,7 +7,13 @@ class ScreenfetchC < Formula
 	sha1 "8d21049fde9a27a1a895ccbd1d782fd28f3908d5"
 
 	def install
-		system "make", "osx", "prefix=#{prefix}"
-		system "make", "install", "prefix=#{prefix}"
+		system "make", "osx"
+		bin.install "src/screenfetch-c"
+		bin.install "src/detectde"
+		bin.install "src/detectwm"
+		bin.install "src/detectwmtheme"
+		bin.install "src/detectgtk"
+		bin.install "src/detectgpu"
+		man.install "manpage/screenfetch-c.1"
 	end
 end
