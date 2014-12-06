5ab3bb08416d0b56b46bba2c29b7038a665070aa
William Woodruff <william@tuffbizz.com>
2014-12-06 11:46:55 -0500

    update homebrew version to 1.3.5

--- screenfetch-c.rb
+++ screenfetch-c.rb
@@ -2,17 +2,12 @@ require "formula"
 
 class ScreenfetchC < Formula
     homepage "http://woodruffw.us/realsite/projects/screenfetch-c.html"
-    url "https://github.com/woodruffw/screenfetch-c/archive/screenfetch-c-1.2.7.tar.gz"
-    sha1 "8f0ae19a0cf3d5bce82a6e583f293fadfddc695f"
+    url "https://github.com/woodruffw/screenfetch-c/archive/screenfetch-c-1.3.5.tar.gz"
+    sha1 "42041290881a7f96cadabefa519b3bc37f1e92fe"
 
     def install
-        system "make", "osx"
+        system "make"
         bin.install "screenfetch-c"
-        bin.install "src/detectde"
-        bin.install "src/detectwm"
-        bin.install "src/detectwmtheme"
-        bin.install "src/detectgtk"
-        bin.install "src/detectgpu"
-        man1.install "manpage/screenfetch-c.1"
+        man1.install "man/screenfetch-c.1"
     end
 end
