a3d4d0893902602c86a2382dfec0e90a4352b5da
William Woodruff <william@tuffbizz.com>
2015-03-21 16:45:36 -0400

    bump formula to 1.4.2

--- screenfetch-c.rb
+++ screenfetch-c.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class ScreenfetchC < Formula
     homepage "http://woodruffw.us/realsite/projects/screenfetch-c.html"
-    url "https://github.com/woodruffw/screenfetch-c/archive/screenfetch-c-1.4.1.tar.gz"
-    sha1 "946030b71cd15b23cf1d2b8e6238d96dd9837a39"
+    url "https://github.com/woodruffw/screenfetch-c/archive/screenfetch-c-1.4.2.tar.gz"
+    sha1 "9cd4387f5fde2ede22b0dfa5c2b60ec04e82a536"
     head "git://github.com/woodruffw/screenfetch-c.git"
 
     def install
