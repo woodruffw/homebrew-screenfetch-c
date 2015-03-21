1f18601135e4eb537d6b20b48aa692cfec03fa66
William Woodruff <william@tuffbizz.com>
2015-03-21 12:04:43 -0400

    update homebrew formula to 1.4.1

--- screenfetch-c.rb
+++ screenfetch-c.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class ScreenfetchC < Formula
     homepage "http://woodruffw.us/realsite/projects/screenfetch-c.html"
-    url "https://github.com/woodruffw/screenfetch-c/archive/screenfetch-c-1.3.7.tar.gz"
-    sha1 "e277ed96121668617d6d903f78bb69f5c49696b8"
+    url "https://github.com/woodruffw/screenfetch-c/archive/screenfetch-c-1.4.1.tar.gz"
+    sha1 "946030b71cd15b23cf1d2b8e6238d96dd9837a39"
     head "git://github.com/woodruffw/screenfetch-c.git"
 
     def install
