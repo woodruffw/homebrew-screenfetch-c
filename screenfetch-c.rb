8dce7a1130f99817b93fbee5d1f54ff803c52a75
William Woodruff <william@tuffbizz.com>
2016-09-29 17:42:48 -0400

    Update formula to screenfetch-c 1.5.0.
    Use tabs instead of spaces, sha256 over sha1.

--- screenfetch-c.rb
+++ screenfetch-c.rb
@@ -1,14 +1,14 @@
 require "formula"
 
 class ScreenfetchC < Formula
-    homepage "http://woodruffw.us/realsite/projects/screenfetch-c.html"
-    url "https://github.com/woodruffw/screenfetch-c/archive/screenfetch-c-1.4.2.tar.gz"
-    sha1 "9cd4387f5fde2ede22b0dfa5c2b60ec04e82a536"
-    head "git://github.com/woodruffw/screenfetch-c.git"
+  homepage "https://github.com/woodruffw/screenfetch-c"
+  url "https://github.com/woodruffw/screenfetch-c/archive/screenfetch-c-1.5.0.tar.gz"
+  sha256 "3f542cc2d1d58ab403e3a8dd137d3ab4722910719b09f1ed9e304f747d561905"
+  head "git://github.com/woodruffw/screenfetch-c.git"
 
-    def install
-        system "make"
-        bin.install "screenfetch-c"
-        man1.install "man/screenfetch-c.1"
-    end
+  def install
+    system "make"
+    bin.install "screenfetch-c"
+    man1.install "man/man1/screenfetch-c.1"
+  end
 end
